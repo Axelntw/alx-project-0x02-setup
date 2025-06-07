06a54ef7c4254b05a0224f550f566002ffa83ea8
Axelntw <ntwaliaxel@gmail.com>
2025-06-07 18:55:27 +0200

    Update

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -19,3 +19,15 @@ export interface CardProps {
   title: string;
   content: string;
 }
+
+export interface PostModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onSubmit: (data: { title: string; content: string }) => void;
+}
+
+export interface Post {
+  id: string;
+  title: string;
+  content: string;
+}
